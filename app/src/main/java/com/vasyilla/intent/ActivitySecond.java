22550a9908bac3fe5c7e9c7f099e76b86f968cdf
unknown <vasyillakafy@gmail.com>
2021-10-15 17:34:27 +0700

    First Commit

---
+++ app/src/main/java/com/vasyilla/intent/ActivitySecond.java
@@ -0,0 +1,22 @@
+package com.vasyilla.intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ActivitySecond extends AppCompatActivity {
+    TextView txtHello;
+    private String nama;
+    private String KEY_NAME = "Nama";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        txtHello = (TextView) findViewById(R.id.txtHello);
+        Bundle extras = getIntent().getExtras();
+        nama = extras.getString(KEY_NAME);
+        txtHello.setText("Hello, " + nama + " !");
+    }
+}
